e9e53454d433ab4e0e66bbe3a1c4774b6dd8e37c
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2025-06-15 15:28:09 -0300

    Resolicao do desafio 'Criando um Sistema e Abstraindo um Celular com POO em C#' por Valdir Torres

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // OK: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // OK: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
     }
 }
